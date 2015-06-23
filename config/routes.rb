4ee0dfcf26622440022555d5618134da70c87385
Rafi Khan <rafi.fa.khan@gmail.com>
2015-06-23 14:52:23 +0200

    styled main page with bootstrap; added a theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root to: 'items#index'
-
   get 'items' => 'items#index', as: :items_path
   post 'items' => 'items#create', as: :create_item
   get 'items/:id' => 'items#show', as: :item
 
   post 'items/:id/buy' => 'items#buy', as: :buy_item
+
+  root to: 'main#index'
 
   # resources :items
   # The above line equals the below 7
